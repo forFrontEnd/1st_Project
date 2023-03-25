18be47c83a168f29aa8d92a6ffb0a2c57e1183df
shyeon0602 <july8560@naver.com>
2023-03-25 20:32:25 +0900

    btn js

--- rspGame/app.js
+++ rspGame/app.js
@@ -1,1 +1,10 @@
 console.log("hi");
+
+//모든 버튼을 buttons 변수로 저장
+const buttons = document.querySelectorAll("button");
+//버튼을 누를 때마다 forEach method로 buttons배열순회반복하여 버튼 내 text를 콘솔창에 출력
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    console.log(event.target.innerText);
+  });
+});
